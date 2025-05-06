9228ea8f62d983611d57931ae0b69150e0486ff1
NateAlmanza2022 <nate2004@uw.edu>
2025-05-06 13:23:22 -0700

    Initial Commit

---
+++ MagzTests/MagzTests.swift
@@ -0,0 +1,17 @@
+//
+//  MagzTests.swift
+//  MagzTests
+//
+//  Created by Nate  on 5/6/25.
+//
+
+import Testing
+@testable import Magz
+
+struct MagzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
